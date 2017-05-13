e8953c1da3a5b153675072416855db2879323328
김휘겸 <breakstorm00@gmail.com>
2017-05-13 22:45:30 +0900

    Modify connect database
     Changes to be committed:
    	modified:   index.js
    	modified:   package.json

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-
+import mysql from 'mysql';
 import router from './routes';
+
 
 const app = express();
 const staticPath = path.resolve(__dirname, 'public');
+const connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'dev',
+	password: '123qwe',
+	database: 'bookflex'
+})
 
+connection.connect();
 app.use(express.static(staticPath));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
