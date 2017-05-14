b2fa1d4044fbf88c6c604164682de8d710ed4223
김휘겸 <breakstorm00@gmail.com>
2017-05-14 16:03:38 +0900

    Modify insert query string problem.
     Changes to be committed:
    	modified:   public/main.html
    	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app = express();
 const router = express.Router();
+const connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'dev',
+	password: '123qwe',
+	database: 'bookflex'
+})
+
 
 app.use(express.static('/Users/gimhwigyeom/Documents/passport-test/public'));
+connection.connect();
 
 router.get('/', function(req,res){
 	console.log("route /router path");
@@ -21,7 +31,30 @@ router.post('/sign', function(req,res){
 		'result': 'ok',
 		'email': req.body
 	}
-	res.send(responseData);
+
+	var email = req.body.email;
+	var responseData = {};
+	//조회문
+	// var query = connection.query('select name, email, password from Users where name="' + email + '"', function(err,rows){
+	// 	if(err) throw err;
+	// 	if(rows[0]){
+	// 		console.log(rows[0]);
+	// 	} else{
+	// 		console.log('none : '+rows[0]);
+	// 	}
+	// })
+	//삽입문
+	var insertQuery = "insert into Users (name, email, password) set ?";
+	var insertData = {
+		name: email,
+		email: email,
+		password: email
+	}
+	var query = connection.query(insertQuery, insertData, function(err, rows){
+		if(err) throw err;
+		console.log("ok db insert")
+	})
+	// res.send(responseData);
 })
 
 export default router;
